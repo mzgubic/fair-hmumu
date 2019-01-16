c424e7d7d348ea3d678298da4ea8650ed1a788de
Miha Zgubic <miha.zgubic@physics.ox.ac.uk>
2019-01-16 13:19:39 +0000

    add entrystop and read dfs

--- fair_hmumu/dataset.py
+++ fair_hmumu/dataset.py
@@ -1,31 +1,51 @@
 import os
+import itertools
 import uproot as ur
 import pandas as pd
 
 
 class DatasetHandler:
-    """
-    Class which handles datasets and splitting into training and test sets.
-    """
 
-    def __init__(self, production, features, nentries=1000):
+    def __init__(self, production, features, entrystop=1000):
+        """
+        Dataset handler. Split in training and test sets, get training batches.
+
+        Args:
+            production (str): Name of the input dataset production
+            features (list of str): List of branch names to be used in the training.
+            entrystop (int or None): Maximum number of read rows. None reads all.
+        """
 
         # settings
         self.production = production
         self.loc = os.path.join(os.getenv('DATA'), production)
         self.features = features
-        self.nentries = nentries
+        self.entrystop = entrystop
         self.datasets = ['sig', 'data', 'ss']
+        self.njets = ['0jet', '1jet', '2jet']
         
         # set up
-        self.setup()
+        self._setup()
 
-    def setup(self):
+    def _setup(self):
 
-        self.df = {}
-        for dataset in self.datasets:
-            #self.df[dataset] = 
-            pass
+        self.df = {ds:{} for ds in self.datasets}
+
+        print('--- Loading the datasets')
+        for dataset, njet in itertools.product(self.datasets, self.njets):
+
+            # get the tree
+            fname = '{}/{}.root'.format(self.loc, dataset)
+            tree = ur.open(fname)[njet]
+
+            # get the array
+            arrays = tree.arrays(branches=self.features, entrystop=self.entrystop)
+
+            # convert it to a DataFrame and decode column names to strings
+            self.df[dataset][njet] = pd.DataFrame(arrays)
+            self.df[dataset][njet].columns = [feature.decode('utf-8') for feature in self.df[dataset][njet].columns]
+
+        
 
 
         
