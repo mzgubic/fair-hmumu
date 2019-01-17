040a0b4f436209b55adddbea52192cfe31292877
Miha Zgubic <miha.zgubic@physics.ox.ac.uk>
2019-01-17 15:20:00 +0000

    comment out the tf for faster testing

--- fair_hmumu/models.py
+++ fair_hmumu/models.py
@@ -1,17 +1,14 @@
-import tensorflow as tf
-import tensorflow.contrib.layers as layers
+#import tensorflow as tf
+#import tensorflow.contrib.layers as layers
+from fair_hmumu.utils import Saveable
 
 
-class Model:
+class Model(Saveable):
 
     def __init__(self, name, hps):
 
         self.name = name
         self.hps = hps
-
-    @classmethod
-    def classname(cls):
-        return cls.__name__
 
     def __str__(self):
         return '{}: {}'.format(self.classname(), str(self.hps))
@@ -21,28 +18,35 @@ class Classifier(Model):
     
     def forward(self, input_layer):
 
-        # build the graph in the scope
-        with tf.variable_scope(self.name):
+        pass 
+        ## build the graph in the scope
+        #with tf.variable_scope(self.name):
 
-            # input layer
-            lay = input_layer
+        #    # input layer
+        #    lay = input_layer
 
-            # hidden layers
-            for layer in range(int(self.hps['depth'])):
-                lay = layers.relu(lay, int(self.hps['n_units']))
+        #    # hidden layers
+        #    for layer in range(int(self.hps['depth'])):
+        #        lay = layers.relu(lay, int(self.hps['n_units']))
 
-            # output layer
-            self.output = layers.linear(lay, self.hps['n_classes'])
+        #    # output layer
+        #    self.output = layers.linear(lay, self.hps['n_classes'])
 
-        these_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=self.name)
+        #these_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=self.name)
 
-        return self.outputs, these_vars
+        #return self.outputs, these_vars
 
     def loss(self, target):
 
-        # build the graph
-        one_hot = tf.one_hot(target, depth=self.hps['n_classes'])
+        pass
+        ## build the graph
+        #one_hot = tf.one_hot(target, depth=self.hps['n_classes'])
 
-        # loss
-        loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=one_hot, logits=self.outputs)
-        loss = tf.math.reduce_mean(loss)
+        ## loss
+        #loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=one_hot, logits=self.outputs)
+        #self.loss = tf.math.reduce_mean(loss)
+
+        #return self.loss
+
+
+
