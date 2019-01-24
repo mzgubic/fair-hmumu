11645b4c5465a725e0381d83de9dddb55075816e
Miha Zgubic <miha.zgubic@physics.ox.ac.uk>
2019-01-24 12:56:28 +0000

    setup_env location fixed

--- setup_env.sh
+++ setup_env.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# for the moment, need to use the most recent nightly build
-source bin/activate
-source /cvmfs/sft-nightlies.cern.ch/lcg/views/dev3python3/latest/x86_64-centos7-gcc62-opt/setup.sh
-export PYTHONPATH="$PYTHONPATH:/home/zgubic/hmumu/fair-hmumu/lib/python3.6/site-packages"
-export PYTHONPATH="$PYTHONPATH:/home/zgubic/hmumu/fair-hmumu"
-
 export SRC="/home/zgubic/hmumu/fair-hmumu"
 export DATA="/data/atlassmallfiles/users/zgubic/hmumu/fair-hmumu/data"
 export RUN="/data/atlassmallfiles/users/zgubic/hmumu/fair-hmumu/run"
+
+# for the moment, need to use the most recent nightly build
+source ${SRC}/bin/activate
+source /cvmfs/sft-nightlies.cern.ch/lcg/views/dev3python3/latest/x86_64-centos7-gcc62-opt/setup.sh
+export PYTHONPATH="$PYTHONPATH:/home/zgubic/hmumu/fair-hmumu/lib/python3.6/site-packages"
+export PYTHONPATH="$PYTHONPATH:/home/zgubic/hmumu/fair-hmumu"
