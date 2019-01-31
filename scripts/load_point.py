f8467add747df1682dff0297e0664108bd50d2d4
Miha Zgubic <miha.zgubic@physics.ox.ac.uk>
2019-01-31 13:13:06 +0000

    add loading script test

---
+++ scripts/load_point.py
@@ -0,0 +1,28 @@
+import os
+import argparse
+import fair_hmumu.trainer
+import fair_hmumu.configuration
+
+
+def main():
+
+    # parse args
+    parser = argparse.ArgumentParser(description='ArgParser')
+    parser.add_argument('-p', '--point',
+                        default=None,
+                        help='Path of the point config')
+    args = parser.parse_args()
+
+    # get the location and configuration file
+    run_conf = fair_hmumu.configuration.Configuration(args.point)
+
+    # make the coach
+    predictor = fair_hmumu.trainer.Predictor(run_conf)
+
+    # load the tf model
+
+    # predict on some data
+
+
+if __name__ == '__main__':
+    main()
